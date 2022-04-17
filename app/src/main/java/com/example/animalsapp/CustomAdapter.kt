505834b4ba08e1abecb3e96ddb67ab0562623613
NickolayMamonov <83017824+NickolayMamonov@users.noreply.github.com>
2022-04-17 11:58:48 +0300

    upd strings.xml

--- app/src/main/java/com/example/animalsapp/CustomAdapter.kt
+++ app/src/main/java/com/example/animalsapp/CustomAdapter.kt
@@ -13,33 +13,25 @@ class CustomAdapter(private val mList: List<Animal>?,
     interface ItemClickListener{
         fun onItemClick(_id: String)
     }
-    // create new views
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        // inflates the card_view_design view
-        // that is used to hold list item
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.recycler_view_design, parent, false)
 
         return ViewHolder(view)
     }
 
-    // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val Animal = mList?.get(position)
-
-        // sets the image to the imageview from our itemHolder class
-        // sets the text to the textview from our itemHolder class
-        holder.textView.text = Animal?.type
+        val animal = mList?.get(position)
+        holder.textView.text = animal?.type
 
     }
 
-    // return the number of the items in the list
     override fun getItemCount(): Int {
         return mList!!.size
     }
 
-    // Holds the views for adding it to image and text
     inner class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val textView: TextView = itemView.findViewById(R.id.textView)
         init {
--- app/src/main/java/com/example/animalsapp/MainActivity.kt
+++ app/src/main/java/com/example/animalsapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.animalsapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.animalsapp.data.Animal
@@ -28,7 +27,8 @@ class MainActivity : AppCompatActivity() {
                 recyclerview.adapter = adapter
             }
 
-            override fun onFailure(call: Call<List<Animal>>, t: Throwable) {
+            override fun onFailure(call: Call<List<Animal>>?, t: Throwable?) {
+
             }
 
             override fun onItemClick(_id: String) {
